d65938c700e8c937bc8f8620ef29f18aa41d0c24
Benjamin Lei <benlei.cse@gmail.com>
2015-11-06 22:23:13 -0800

    I don't think DNSS needs a complicated routing system. Adding in nginx files for future references. Tested templating with jade.

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-
-var routes = require('./routes/index');
-
-var app = express();
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var app = express()
+var router = express.Router()
+var fs = require('fs')
+module.exports = app;
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(cookieParser());
-app.use('/', routes);
+app.use(cookieParser())
+app.use('/', router)
 
-module.exports = app;
+fs.readFile('/var/www/json/jobs.json', 'utf8', function(err, data) {
+    if (err) throw err
+    jobs = JSON.parse(data)
+})
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Expressive', jobs: jobs })
+})
+
+
