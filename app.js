8142e3f5912334cdfc35197e22bda7e3091f9f00
Benjamin Lei <benlei.cse@gmail.com>
2015-11-08 23:11:06 -0800

    Adding in a sample dnss.json.
    With new changes to the json files + db.json, don't need to load up each json file anymore.

--- app.js
+++ app.js
@@ -4,25 +4,56 @@ var cookieParser = require('cookie-parser')
 var app = express()
 var router = express.Router()
 var fs = require('fs')
+var util = require('util')
+
 module.exports = app;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(cookieParser())
 app.use('/', router)
 
-fs.readFile('/var/www/json/jobs.json', 'utf8', function(err, data) {
-    if (err) throw err
-    jobs = JSON.parse(data)
+// settings json
+var settings = JSON.parse(fs.readFileSync('/dnss/dnss.json', 'utf8'))
+
+//=======================================
+// CUSTOM DATA GATHERING INTO MEM
+//=======================================
+// database of stuff
+var db
+
+// skills and skilltrees
+var skills = {}
+
+// route jobs
+var routeJobs = {}
+
+// the last job = show in index page
+var lastJobID = 0
+
+fs.readFile(settings.db, 'utf8', function(err, $) {
+  if (err) throw err
+  db = JSON.parse($)
+
+  for (jobID in db.Jobs) {
+    var job = db.Jobs[jobID]
+    if (lastJobID < jobID) { // used to redirect to newest class
+      lastJobID = jobID
+    }
+        
+    if (job.JobNumber == 2) { // keep track of 2nd adv eng name for faster lookup
+      routeJobs[job.EnglishName] = jobID
+    }
+  }
 })
 
 /* GET home page. */
-router.get('/', function(req, res) {
-    res.render('index', { title: 'Expressive', jobs: jobs })
+router.get('/:job([a-z]+)-:level([1-9][0-9]*)', function(req, res) {
+  if (!routeJobs[req.params.job]) throw "job " + req.params.job + " not found"
+
+  res.render('index', { title: 'Dragon Nest Skill Simulator', db: db, settings: settings })
 })
 
 
