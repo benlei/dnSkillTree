f3399ea8c8c39e5a153eb77cfc48da259bc6a234
Benjamin Lei <benlei.cse@gmail.com>
2015-11-15 20:33:29 -0800

    Also need the added skill.js

---
+++ public/js/skill.js
@@ -0,0 +1,12 @@
+function bind_skill(id, dom) {
+  dom.bind({
+    mousedown: function(e) {
+      var shiftOrCtrl = e.shiftKey || e.ctrlKey;
+      
+    },
+
+    mouseenter: function() {
+
+    }
+  })
+}
