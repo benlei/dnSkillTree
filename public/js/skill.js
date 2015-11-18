45312cf6edcd6a88af775af63079eb19d8693e05
Benjamin Lei <benlei.cse@gmail.com>
2015-11-17 21:16:03 -0800

    SP is now calculated towards the job. Cannot go past max SP of job anymore either.

--- public/js/skill.js
+++ public/js/skill.js
@@ -6,31 +6,49 @@ function skill_adj(e) {
   var skill = db.Skills[dom.data('skill')];
   dom.find('.skill-bdr').removeClass('gray');
 
-  var curr = lvl[0];
+  var prev = lvl[0];
   if (e.button == 0) { // left click
     lvl[0] = Math.min(lvl[1], max ? lvl[1] : lvl[0] + 1);
-    dom.css('background-image', image);
   } else if (e.button == 2) { // right click
     lvl[0] = Math.max(0, max ? 0 : lvl[0] - 1);
     if (skill.Levels[1].LevelLimit == 1 && lvl[0] == 0) { // default case
       lvl[0] = 1;
     }
+  }
 
-    if (lvl[0] == 0) {
-      dom.css('background-image', image.replace('.png', '_b.png'));
-      dom.find('.skill-bdr').addClass('gray');
+  var spdom = dom.closest('.panel').find('.panel-heading span'); // do something later
+  var sp = spdom.text().split('/').map(int);
+
+  // find SP difference
+  var diff = 0, end = Math.max(prev, lvl[0]), inc = prev < lvl[0];
+  for (var i = Math.min(prev, lvl[0]) + 1; i <= end; i++) {
+    var s = skill.Levels[i].SkillPoint;
+    if (inc) {
+      if (sp[0] + diff + s > sp[1]) {
+        lvl[0] = i - 1;
+        break;
+      }
+
+      diff += s;
+    } else {
+      diff -= s;
     }
   }
 
-  dom.data('lvl', lvl.join(','));
-  dom.find('.skill-lvl').text(lvl.join('/'));
-
-  // find SP difference
-  var diff = 0;
+  if (prev == lvl[0]) {
+    return; // do nothing
+  }
 
   // SP adjustment
-  var spdom = dom.closest('.panel').find('.panel-heading span'); // do something later
-  var sp = spdom.text().split('/').map(int);
+  sp[0] += diff;
+
+  // icon update
+  dom.css('background-image', lvl[0] ? image : image.replace('.png', '_b.png'));
+  lvl[0] || dom.find('.skill-bdr').addClass('gray');
+  spdom.text(sp.join('/'));
+
+  dom.data('lvl', lvl.join(','));
+  dom.find('.skill-lvl').text(lvl.join('/'));
 }
 
 function skill_description() {
