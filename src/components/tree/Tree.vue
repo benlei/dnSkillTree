a23330f68ac126f7896a071f159459a225f0d569
Benjamin Lei <benlei.cse@gmail.com>
2017-01-24 23:46:25 -0800

    Small progress!

--- src/components/tree/Tree.vue
+++ src/components/tree/Tree.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="container">
-    <Navigation />
+    <Navigation/>
 
     <div class="row">
       <div class="col-md-4">
         Hello, {{ $route.params.slug }}! This is for the selector and stuff.
       </div>
       <div class="col-md-4">
-        This is the skill tree? {{ testData }}
+        <table class="tree">
+          <tr v-for="row in 6">
+            <td v-for="col in 4">
+              <Skill :id="getSkillId(row, col)" />
+            </td>
+          </tr>
+        </table>
       </div>
       <div class="col-md-4">
         This is the description.
@@ -19,6 +25,7 @@
 <script>
   import { mapState } from 'vuex';
   import Navigation from '../Navigation';
+  import Skill from './Skill';
 
   export default {
     created() {
@@ -26,13 +33,33 @@
     },
     computed: {
       ...mapState({ job: 'job' }),
-      testData() {
-        console.log(this.job.tree);
-        return this.job.tree.length;
+      getSkillId(row, col) {
+        row -= 1;
+        col -= 1;
+        return this.job.tree[(row * 4) + col];
       },
     },
     components: {
       Navigation,
+      Skill,
     },
   };
 </script>
+
+<style>
+  /*.skill-table {*/
+    /*margin: 0 auto 15px auto;*/
+    /*background-repeat: no-repeat;*/
+    /*background-position: -5px -7px;*/
+    /*background-size: 317px 510px;*/
+  /*}*/
+
+  /*.skill-table .skill-tr .skill-td {*/
+    /*padding: 0 10px 28px 10px;*/
+  /*}*/
+
+  /*.skill {*/
+    /*height: 50px;*/
+    /*width: 50px;*/
+  /*}*/
+</style>
