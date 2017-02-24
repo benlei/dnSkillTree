3c4d90040d0c4830b8cde7b5c3002d506e569ae3
Benjamin Lei <benlei.cse@gmail.com>
2017-02-24 11:31:53 -0800

    #35 - addressed the 1 one (techs should be listed line by line).

--- src/components/common/Techniques.vue
+++ src/components/common/Techniques.vue
@@ -1,17 +1,19 @@
 <template>
   <div class="d-flex justify-content-between">
     <div class="d-inline unselectable">
-      <span class="tech" v-for="tech in skill.techs" @click.prevent="gearTech(tech)">
-        <a href="javascript:;" class="fa"
-           :class="{ 'fa-circle-thin': !isTeched(tech), 'fa-circle': isTeched(tech) }"
-        /> {{ tech | techName }}
-      </span>
+      <ul class="tech">
+        <li v-for="tech in skill.techs" @click.prevent="gearTech(tech)">
+          <a href="javascript:;" class="fa"
+             :class="{ 'fa-circle-thin': !isTeched(tech), 'fa-circle': isTeched(tech) }"
+          /><a href="javascript:;" class="text-black"> {{ tech | techName }}</a>
+        </li>
+      </ul>
     </div>
 
     <div class="crest-tech unselectable" @click.prevent="crestTech">
       <a href="javascript:;" class="fa"
          :class="{ 'fa-circle-thin': !isTeched(0), 'fa-circle': isTeched(0) }"
-      /> Crest
+      /><a href="javascript:;" class="text-black">Crest</a>
     </div>
   </div>
 </template>
@@ -24,22 +26,28 @@
   };
 </script>
 
-<style scoped>
-  .card .card-block + .card-block {
-    padding-top: 0;
-  }
-</style>
 
 <style>
   .tech {
-    margin-right: 1.25rem;
+    list-style-type: none;
+    padding-left: 0;
+    margin-left: 3px;
+    margin-bottom: 0;
   }
 
   .tech .fa, .crest-tech .fa {
     margin-right: 3px;
   }
 
+  .fa span {
+    font: inherit !important;
+  }
+
   a.fa {
     color: inherit !important;
+  }
+
+  .text-black {
+    color: #000 !important;
   }
 </style>
